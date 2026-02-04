f4f5114a282553c43a1cd37a18696f1460295606
Marlon Felix <contatomarlonfelix@hotmail.com>
2026-02-04 14:30:07 -0300

    feat: export components ui

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from "./Button";
+export * from "./Card";
+export * from "./Container";
+export * from "./Input";
+export * from "./Radius";
+export * from "./Separator";
+export * from "./Typography";
