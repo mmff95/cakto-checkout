c505c5781f3eb505a9fbb786d4f62d5e150b3818
Marlon Felix <contatomarlonfelix@hotmail.com>
2026-02-04 15:40:43 -0300

    feat: import common components form checkout

---
+++ src/components/common/index.ts
@@ -0,0 +1,8 @@
+export { CheckoutProductCard } from "./CheckoutProductCard";
+export { CheckoutPersonalDataCard } from "./CheckoutPersonalDataCard";
+export { CheckoutPaymentCard } from "./CheckoutPaymentCard";
+export { CheckoutOrderSummaryCard } from "./CheckoutOrderSummaryCard";
+
+export type { CheckoutProductCardProps } from "./CheckoutProductCard";
+export type { PaymentMethod } from "./CheckoutPaymentCard";
+export type { CheckoutOrderSummaryCardProps } from "./CheckoutOrderSummaryCard";
